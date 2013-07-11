b2cfba106c4d0df159b1d3e341fc600892de92ee
hminaya <hminaya@gmail.com>
2013-07-10 20:17:51 -0400

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(:default, Rails.env)
 
 module DataDevelopersDo
   class Application < Rails::Application
+
+    config.generators do |g|
+      
+      
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
