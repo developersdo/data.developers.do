416fefdf3403ae47cfe0d8f48aee694182660544
hminaya <hminaya@gmail.com>
2013-07-28 13:39:07 -0400

    Ahora esta el ejemplo con JSON, XML, CSV y XLS

--- app/controllers/api/v1/provincias_controller.rb
+++ app/controllers/api/v1/provincias_controller.rb
@@ -1,10 +1,17 @@
 module Api
 	module V1
 		class ProvinciasController < ApplicationController
-			respond_to :json
+			respond_to :json, :xml, :csv, :xls
 
 			def index
-				respond_with Provincia.all.to_json
+				@provincias = Provincia.all
+
+				respond_to do |format|
+					format.json {render json: @provincias.to_json()}
+					format.xml {render xml: @provincias}
+					format.csv {render text: @provincias.to_csv}
+					format.xls {render text: @provincias.to_csv(col_sep: "\t")}
+				end
 			end
 		end
 	end
